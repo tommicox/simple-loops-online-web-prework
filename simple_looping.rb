0b6056187476c6fccb1ff4ed6e50801651acbd33
Tommi Cox <tommicox@gmail.com>
2019-03-02 20:31:28 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,7 +12,7 @@ def loop_iterator(number_of_times)
   counter = 0
   loop do
     counter += 1
-  break
+  break if counter == number_of_times
 end
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
