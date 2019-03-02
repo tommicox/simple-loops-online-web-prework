d83b465aa10c3968b998a31a582dc0cd164adc5f
Tommi Cox <tommicox@gmail.com>
2019-03-02 20:30:05 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,7 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
   loop do
-    puts phrase
+    counter
     answer = gets.chomp
     break
 end
