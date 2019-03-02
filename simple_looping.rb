db2d1955a723bc4a0d8d492d171168f0bc95d69f
Tommi Cox <tommicox@gmail.com>
2019-03-02 20:33:11 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -17,7 +17,7 @@ def loop_iterator(number_of_times)
 end
 
 def times_iterator(number_of_times)
-  # code your solution here using the "times" keyword
+  number_of_times.times do
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
 end
